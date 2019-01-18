05dfc52700da21f461a05d2df2aff7172aae3c53
Daren Cantrell <daren.cantrell@supplypointsystems.com>
2019-01-18 11:19:36 +0000

    Event Details added

--- Events.Web/Controllers/BaseController.cs
+++ Events.Web/Controllers/BaseController.cs
@@ -1,10 +1,19 @@
 ﻿namespace Events.Web.Controllers
 {
     using Events.Data;
+    using Microsoft.AspNet.Identity;
     using System.Web.Mvc;
 
     public class BaseController : Controller
     {
         protected ApplicationDbContext db = new ApplicationDbContext();
+
+        public bool IsAdmin()
+        {
+            var currentUserId = this.User.Identity.GetUserId();
+            var isAdmin = (currentUserId != null && this.User.IsInRole("Administrator"));
+
+            return isAdmin;
+        }
     }
 }
--- Events.Web/Controllers/HomeController.cs
+++ Events.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿namespace Events.Web.Controllers
 {
     using Events.Web.Models;
+    using Microsoft.AspNet.Identity;
     using System;
     using System.Linq;
     using System.Web.Mvc;
@@ -22,6 +23,22 @@
                 UpcomingEvents = upcomingEvents,
                 PassedEvents = passedEvents
             });
+        }
+
+        public ActionResult EventDetailsById(int id)
+        {
+            var currentUserId = this.User.Identity.GetUserId();
+            var isAdmin = this.IsAdmin();
+            var eventDetails = this.db.Events
+                .Where(e => e.Id == id)
+                .Where(e => e.IsPublic || isAdmin || (e.AuthorId != null && e.AuthorId == currentUserId))
+                .Select(EventDetailsViewModel.ViewModel)
+                .FirstOrDefault();
+
+            var isOwner = (eventDetails != null && eventDetails.AuthorId != null && eventDetails.AuthorId == currentUserId);
+            this.ViewBag.CanEdit = isOwner || isAdmin;
+
+            return this.PartialView("_EventDetails", eventDetails);
         }
     }
 }
